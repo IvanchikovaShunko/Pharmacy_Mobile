85fd283a542944e4b518363e0a8f1022e6504710
annashunko <annshunko@gmail.com>
2016-11-24 22:42:57 +0300

    init project

---
+++ app/src/main/java/bsu/fpmi/pharmacy/pharmacy_mobile/MainActivity.java
@@ -0,0 +1,13 @@
+package bsu.fpmi.pharmacy.pharmacy_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
