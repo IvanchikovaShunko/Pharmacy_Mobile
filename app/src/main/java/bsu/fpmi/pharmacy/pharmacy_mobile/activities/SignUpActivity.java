628c2d70d62fd8527d2764e104963ec54a2842a0
annashunko <annshunko@gmail.com>
2016-12-05 23:59:02 +0300

    sign up layout modified

--- app/src/main/java/bsu/fpmi/pharmacy/pharmacy_mobile/activities/SignUpActivity.java
+++ app/src/main/java/bsu/fpmi/pharmacy/pharmacy_mobile/activities/SignUpActivity.java
@@ -2,14 +2,37 @@ package bsu.fpmi.pharmacy.pharmacy_mobile.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import bsu.fpmi.pharmacy.pharmacy_mobile.R;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        spinner = (Spinner) findViewById(R.id.spinnerGender);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
